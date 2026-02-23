6b7271f29a6d6ca0fd278e09a5fe99938a7a625b
enotbay <grunden.ethan@gmail.com>
2026-02-22 23:01:31 -0500

    Add functions to create room light group and light cards

--- google-home-strategy-cards.js
+++ google-home-strategy-cards.js
@@ -1,1 +1,67 @@
-
+function createRoomLightGroupCard (roomGroup) {
+  return (
+    {
+      "type": "custom:expander-card",
+      "title-card": {
+          "type": "custom:material-slider-card",
+          "entity": roomGroup.entity_id,
+          "icon": "m3of:lightbulb",
+          "show_percentage": true,
+          "bold_text": false,
+          "control_type": "light"
+      },
+      "clear-children": true,
+      "title-card-button-overlay": true,
+      "title-card-clickable": false,
+      "child-margin-top": ".6em",
+      "clear": true,
+      "button-background": "black",
+      "padding": "0",
+      "overlay-margin": "20px",
+      "cards": [
+        {
+          "type": "custom:auto-entities",
+          "card": {
+            "square": false,
+            "type": "grid",
+            "columns": 2
+          },
+          "card_param": "cards",
+          "filter": {
+            "include": [
+              {
+                "group": {
+                  "group": roomGroup.entity_id,
+                  "active_choice": "group"
+                },
+                "options": {
+                  "type": "custom:material-slider-card",
+                  "show_percentage": true,
+                  "bold_text": false,
+                  "control_type": "light",
+                  "show_percentage": true,
+                }
+              }
+            ],
+            "exclude": []
+          },
+          "show_empty": false,
+          "sort": {
+            "method": "entity_id"
+          }
+        }
+      ]
+    }
+  );
+}
+function createLightCard (entity) {
+  return (
+    {
+      "type": "custom:material-slider-card",
+      "show_percentage": true,
+      "bold_text": false,
+      "control_type": "light",
+      "show_percentage": true,
+    }
+  );
+}
